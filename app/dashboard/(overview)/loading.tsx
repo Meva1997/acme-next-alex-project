a5dc60675dfddaefc71da8e1f921da0a3051306c
Alex Medina <alexmedina@192.168.1.238>
2025-07-11 11:40:55 -0600

    fix

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -3,3 +3,4 @@ import DashboardSkeleton from "@/app/ui/skeletons";
 export default function Loading() {
   return <DashboardSkeleton />;
 }
+// This file is used to show a loading state while the dashboard data is being fetched.
